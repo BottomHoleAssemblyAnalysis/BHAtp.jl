b0f6cc3bb5d18f833d2d84741ebceacf462bf2e7
Rob J Goedman <goedman@mac.com>
2019-12-10 18:50:12 -0700

    Updated savefif.jl

--- src/Savefig.jl
+++ src/Savefig.jl
@@ -1,9 +1,10 @@
 function savefig(figname, obj, pdir=mktempdir())
   println(pdir)
-  fexts = [".pdf", ".svg", ".tex"]
+  fexts = [".png", ".pdf", ".svg", ".tex"]
   for ext in fexts
     isfile(figname * ext) && rm(figname * ext)
   end
+  pgfsave( joinpath(pdir, figname * ".png"), obj)
   pgfsave( joinpath(pdir, figname * ".pdf"), obj)
   run(`pdf2svg $(joinpath(pdir, figname * ".pdf")) $(joinpath(pdir, figname * ".svg"))`)
   pgfsave(joinpath(pdir, figname * ".tex"), obj);
